16466505e1c31d27713d51b6bfbfa3fb16cc7f70
Abdul Basit <chabdulbasit1122@gmail.com>
2020-09-01 04:20:19 +0500

    Fixing a minor bug

--- src/Components/Accounts.js
+++ src/Components/Accounts.js
@@ -20,7 +20,6 @@ const Accounts = (props) => {
 
           <div className="col d-flex justify-content-center ">
             <Link
-              
               to="/account/FactoryAccounts"
               type="button"
               className="btn btn-dark con bts"
